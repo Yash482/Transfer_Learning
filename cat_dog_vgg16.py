5f62c00c79811de0535629c9af84a08da8518387
Yash Bansal <agarwaly482@gmail.com>
2020-08-22 12:07:16 +0530

    load the images and scale them

---
+++ cat_dog_vgg16.py
@@ -0,0 +1,36 @@
+import glob
+import numpy as np
+import os
+import shutil
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
+np.random.seed(42)
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+#get dataset
+   
+IMG_DIM = (150, 150)
+
+train_files = glob.glob('training_data/*')
+train_imgs = [img_to_array(load_img(img, target_size=IMG_DIM)) for img in train_files]
+train_imgs = np.array(train_imgs)
+train_labels = [fn.split('\\')[1].split('.')[0].strip() for fn in train_files]
+
+validation_files = glob.glob('validation_data/*')
+validation_imgs = [img_to_array(load_img(img, target_size=IMG_DIM)) for img in validation_files]
+validation_imgs = np.array(validation_imgs)
+validation_labels = [fn.split('\\')[1].split('.')[0].strip() for fn in validation_files]
+
+print('Train dataset shape:', train_imgs.shape, 
+      '\tValidation dataset shape:', validation_imgs.shape)
+
+#scale img pixels
+train_imgs_scaled = train_imgs.astype('float32')
+validation_imgs_scaled  = validation_imgs.astype('float32')
+train_imgs_scaled /= 255
+validation_imgs_scaled /= 255
+
+print(train_imgs[0].shape)
+array_to_img(train_imgs[0])
+#==================================================================
